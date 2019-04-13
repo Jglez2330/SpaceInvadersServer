0978fab0498a3133ca3c7be7f6ad8ac68876c77d
Joseph Gonzalez <josegonzalez2330@gmail.com>
2019-04-13 05:27:32 -0600

    Auto stash before merge of "master" and "origin/master"

---
+++ cmake-build-debug/CMakeFiles/SpaceInvadersServer.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpaceInvadersServer.dir/main.c.o"
+  "CMakeFiles/SpaceInvadersServer.dir/Socket.c.o"
+  "CMakeFiles/SpaceInvadersServer.dir/Server.c.o"
+  "SpaceInvadersServer.pdb"
+  "SpaceInvadersServer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SpaceInvadersServer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
